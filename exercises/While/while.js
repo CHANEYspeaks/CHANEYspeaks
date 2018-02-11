aeb8cf00b1eeabefeaeb0bbea0c33f83c20ec9d8
Christopher Chaney <CHANEYspeaks@gmail.com>
2018-02-11 16:18:48 -0500

    lots of stuff

---
+++ exercises/While/while.js
@@ -0,0 +1,7 @@
+var n = 0
+console.log("I am called the Count... because I really love to count!")
+while (n < 10) {
+  console.log(n, "ha-ha-ha")
+  n++
+}
+console.log('fin!')
---
+++ projects/RPS/rps.js
@@ -0,0 +1,1 @@
+var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
