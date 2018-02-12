ea81fb120148e0bcfd31eb326cd9a681e8bd8403
Christopher Chaney <CHANEYspeaks@gmail.com>
2018-02-11 20:08:19 -0500

    new again

--- projects/CYOA/cyoa.js
+++ projects/CYOA/cyoa.js
@@ -8,3 +8,12 @@ if(response === "Mentor"){
 } else {
     alert("Make a decision so you can move forward. Refresh the page to try again.");
 }
+var response = prompt("After a long night of trouble-shooting your coding for hours, you get stuck with a question. On the next day, you walk into your coding class eager to get some direction for solving your problem. You see Warren and your mentor. Type 'Warren' to ask him, type 'Mentor' to ask her");
+
+if(response === "Mentor"){
+    response = prompt("I am happy to help you, anytime!");
+} else if (response === "Warren") {
+    response = prompt("What! You need help again! DAMN!!!!");
+} else {
+    alert("Make a decision so you can move forward. Refresh the page to try again.");
+}
