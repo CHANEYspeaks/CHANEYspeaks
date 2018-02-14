c681bbd387012a82df43e8b75a5231d9f2c5b7d7
Christopher Chaney <CHANEYspeaks@gmail.com>
2018-02-13 20:16:18 -0500

    add new

--- projects/CYOA/cyoa.js
+++ projects/CYOA/cyoa.js
@@ -1,19 +1,27 @@
 
+// var response = prompt("After a long night of trouble-shooting your coding challenge, you get stuck with a question. On the next day, you walk into your coding class eager to get some direction for solving your problem. You see Warren and your mentor. Type 'Warren' to ask him, type 'Mentor' to ask her");
+//
+// if(response === "Mentor"){
+//     alert("I am happy to help you, anytime!");
+// }
+// else if (response === "Warren") {
+//     prompt("Yes or No.  Do you really need some help?");
+// }
+// else if(response === "No") {
+//   alert ("I am just joking.")
+// }
+// else if (response === "Yes") {
+// alert ("Yes, I am needing help.")
+// }
+//  else {
+//     alert("Refresh the page and choose an option try again.");
+
 var response = prompt("After a long night of trouble-shooting your coding for hours, you get stuck with a question. On the next day, you walk into your coding class eager to get some direction for solving your problem. You see Warren and your mentor. Type 'Warren' to ask him, type 'Mentor' to ask her");
 
 if(response === "Mentor"){
-    response = prompt("I am happy to help you, anytime!");
+    alert("I am happy to help you, anytime!");
 } else if (response === "Warren") {
-    response = prompt("What! You need help again! DAMN!!!!");
-} else {
-    alert("Make a decision so you can move forward. Refresh the page to try again.");
-}
-var response = prompt("After a long night of trouble-shooting your coding for hours, you get stuck with a question. On the next day, you walk into your coding class eager to get some direction for solving your problem. You see Warren and your mentor. Type 'Warren' to ask him, type 'Mentor' to ask her");
-
-if(response === "Mentor"){
-    response = prompt("I am happy to help you, anytime!");
-} else if (response === "Warren") {
-    response = prompt("What! You need help again! DAMN!!!!");
+    alert("What! You need help again! DAMN!!!!");
 } else {
     alert("Make a decision so you can move forward. Refresh the page to try again.");
 }
